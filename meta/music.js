b9167a17f8cd6306a46db300bf583ab63776861b
ChrisplayzWii <191655784+ChrisplayzYT@users.noreply.github.com>
2025-04-05 08:53:44 -0500

    Add files via upload

--- meta/music.js
+++ meta/music.js
@@ -5,21 +5,27 @@ var isWednesday = new Date().getDay() === 3;
 var shoploop = new Audio(isWednesday ? "/media/shop_bgm_loop_wsw.wav" : "/media/shop_bgm_loop.wav");
 shoploop.loop = true;
 shoploop.volume = 0.8;
+
 window.onload = function() {
-
   var savedTime = localStorage.getItem("bgmlooppoint");
-
 
   if (savedTime) {
     shoploop.currentTime = parseFloat(savedTime); 
   }
 
   if (localStorage.getItem("shopmusic") === "playing") {
-      playBGM();
+    playBGM();
   } else {
     pauseBGM();
   }
 
+  if (isWednesday) {
+    document.getElementById("wednesdaytitles").style.display = "block";
+    document.getElementById("wednesdaytitles").style.height="fit-content";
+  } else {
+    document.getElementById("wednesdaytitles").style.display = "none";
+    document.getElementById("wednesdaytitles").style.height="0px";
+  }
 };
 
 window.onbeforeunload = function() {
@@ -28,20 +34,14 @@ window.onbeforeunload = function() {
 
 function playBGM() {
   localStorage.setItem("shopmusic", "playing");
-
   shoploop.play();
-  document.getElementById("shopbgm").innerHTML="Pause BGM";
-  document.getElementById("shopbgmselector").href="javascript:pauseBGM();"
+  document.getElementById("shopbgm").innerHTML = "Pause BGM";
+  document.getElementById("shopbgmselector").href = "javascript:pauseBGM();";
 }
-
 
 function pauseBGM() {
   localStorage.setItem("shopmusic", "paused");
-
   shoploop.pause();
-  document.getElementById("shopbgm").innerHTML="Play BGM";
-  document.getElementById("shopbgmselector").href="javascript:playBGM();"
+  document.getElementById("shopbgm").innerHTML = "Play BGM";
+  document.getElementById("shopbgmselector").href = "javascript:playBGM();";
 }
-
-
-// wii shop theme (suggested by Le gamer66 on discord URL: https://discord.com/channels/1346485785284575335/1346485786039681056/1351527080546009259)
